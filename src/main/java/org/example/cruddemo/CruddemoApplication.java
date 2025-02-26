49d17be024682c520abfe48e91d475807a7dc921
Teresa <teresagalaviza@gmail.com>
2025-02-25 20:54:11 -0700

    CreateMultipleStudents method added

--- src/main/java/org/example/cruddemo/CruddemoApplication.java
+++ src/main/java/org/example/cruddemo/CruddemoApplication.java
@@ -17,8 +17,24 @@ public class CruddemoApplication {
     @Bean
     public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            createStudent(studentDAO);
+            //createStudent(studentDAO);
+            
+            createMultipleStudents(studentDAO);
         };
+    }
+
+    private void createMultipleStudents(StudentDAO studentDAO) {
+        // create multiple students
+        System.out.println("Creating 3 new student objects...");
+        Student temptStudent1 = new Student("Jose", "Torres", "jose@gmail.com");
+        Student temptStudent2 = new Student("Kiel", "Crew", "kiel@gmail.com");
+        Student temptStudent3 = new Student("Ariel", "Hugson", "ariel@gmail.com");
+
+        // save the student objects
+        System.out.println("Saving the students...");
+        studentDAO.save(temptStudent1);
+        studentDAO.save(temptStudent2);
+        studentDAO.save(temptStudent3);
     }
 
     private void createStudent(StudentDAO studentDAO) {
